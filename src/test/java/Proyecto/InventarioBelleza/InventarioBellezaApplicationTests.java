0ccaa280aea3a4cc5340edd5f425c8584dbeccac
Pool Reategui <reateguiaguilarpoolalexander@gmail.com>
2026-04-04 22:09:29 -0500

    Agregando el proyecto final

---
+++ src/test/java/Proyecto/InventarioBelleza/InventarioBellezaApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto.InventarioBelleza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioBellezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
